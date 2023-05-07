59a0f2a0145f1f2cb83afc602e2cb5ab43a4b6d4
balazs4 <balazs4web@gmail.com>
2023-05-07 20:35:04 +0200

    use if let instead match-arms

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,11 @@ fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let l = line.unwrap();
-        match counter.get_mut(&l) {
-            Some(value) => *value += 1,
-            None => {
-                counter.insert(l, 1);
-                ()
-            }
-        };
+        if let Some(value) = counter.get_mut(&l) {
+            *value += 1;
+            continue;
+        }
+        counter.insert(l, 1);
     }
 
     for (key, value) in counter.iter() {
